d4ab6e3aebe97de7f6a9ed8228027aafb7315c99
Mrick343 <mrinmoy8429de@gmail.com>
2025-09-23 18:33:28 +0000

    Add EuclidOS info

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -181,23 +181,34 @@ var BannerVars = []string{
 }
 
 func Banner(config Config, make_vars map[string]string) string {
-	b := &bytes.Buffer{}
+    b := &bytes.Buffer{}
 
-	fmt.Fprintln(b, "============================================")
-	for _, name := range BannerVars {
-		if make_vars[name] != "" {
-			fmt.Fprintf(b, "%s=%s\n", name, make_vars[name])
-		}
-	}
-	if config.skipKatiControlledByFlags {
-		fmt.Fprintf(b, "SOONG_ONLY=%t\n", config.soongOnlyRequested)
-	} else { // default for this product
-		fmt.Fprintf(b, "SOONG_ONLY=%t\n", make_vars["PRODUCT_SOONG_ONLY"] == "true")
-	}
+    fmt.Fprintln(b, "==================================================================================")
+    fmt.Fprintln(b, "                                                                                  ")
+    fmt.Fprintln(b, "  ███████╗██╗   ██╗ ██████╗██╗     ██╗██████╗      ██████╗ ███████╗ ")
+    fmt.Fprintln(b, "  ██╔════╝██║   ██║██╔════╝██║     ██║██╔══██╗    ██╔═══██╗██╔════╝ ")
+    fmt.Fprintln(b, "  █████╗  ██║   ██║██║     ██║     ██║██║  ██║    ██║   ██║███████╗ ")
+    fmt.Fprintln(b, "  ██╔══╝  ██║   ██║██║     ██║     ██║██║  ██║    ██║   ██║╚════██║ ")
+    fmt.Fprintln(b, "  ███████╗╚██████╔╝╚██████╗███████╗██║██████╔╝    ╚██████╔╝███████║ ")
+    fmt.Fprintln(b, "  ╚══════╝ ╚═════╝  ╚═════╝╚══════╝╚═╝╚═════╝      ╚═════╝ ╚══════╝ ")
+    fmt.Fprintln(b, "                                                                                  ")
+    fmt.Fprintln(b, "==================================================================================")
+    fmt.Fprintf(b, "%s = %s\n", "EUCLID_VERSION", make_vars["EUCLID_VERSION"])
+    fmt.Fprintf(b, "%s = %s\n", "EUCLID_BUILD_TYPE", make_vars["EUCLID_BUILD_TYPE"])
+    fmt.Fprintf(b, "%s = %s\n", "PLATFORM_VERSION", make_vars["PLATFORM_VERSION"])
+    fmt.Fprintf(b, "%s = %s\n", "TARGET_PRODUCT", make_vars["TARGET_PRODUCT"])
+    fmt.Fprintf(b, "%s = %s\n", "TARGET_BUILD_VARIANT", make_vars["TARGET_BUILD_VARIANT"])
+    fmt.Fprintf(b, "%s = %s\n", "TARGET_ARCH", make_vars["TARGET_ARCH"])
+    fmt.Fprintf(b, "%s = %s\n", "TARGET_ARCH_VARIANT", make_vars["TARGET_ARCH_VARIANT"])
+    fmt.Fprintf(b, "%s = %s\n", "TARGET_CPU_VARIANT", make_vars["TARGET_CPU_VARIANT"])
+    fmt.Fprintf(b, "%s = %s\n", "TARGET_2ND_ARCH", make_vars["TARGET_2ND_ARCH"])
+    fmt.Fprintf(b, "%s = %s\n", "BUILD_ID", make_vars["BUILD_ID"])
+    fmt.Fprintf(b, "%s = %s\n", "PRODUCT_DEFAULT_DEV_CERTIFICATE", make_vars["PRODUCT_DEFAULT_DEV_CERTIFICATE"])
+    fmt.Fprintf(b, "%s = %s\n", "OUT_DIR", make_vars["OUT_DIR"])
+    fmt.Fprintf(b, "%s=%s\n", "PRODUCT_SOONG_NAMESPACES", make_vars["PRODUCT_SOONG_NAMESPACES"])
+    fmt.Fprintln(b, "===============================================================")
 
-	fmt.Fprint(b, "============================================")
-
-	return b.String()
+    return b.String()
 }
 
 func runMakeProductConfig(ctx Context, config Config) {
